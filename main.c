f7d38d2948edd92e3b7bac7841f9498d50b1a1d8
Maydit <Dawiided@gmail.com>
2019-04-22 16:24:05 -0400

    Add gaussian and layout

--- main.c
+++ main.c
@@ -20,11 +20,17 @@
 
 //defines
 #define NUM_TICKS 1000 //Number of ticks
-#ifdef F2
-#define MM_Dist matchmaking_dist_f2
+
+#ifdef ELO
+#define MM_Dist matchmaking_dist_elo
+#define START_MMR 1200
+#define nextmmr next_mmr_elo
 #else
-#define MM_Dist matchmaking_dist_f1
+#define MM_Dist matchmaking_dist_trueskill
+#define START_MMR 0 //CHANGE THIS
+#define nextmmr next_mmr_trueskill
 #endif
+
 typedef struct Player {
   int mmr;
   unsigned int ping;
@@ -35,8 +41,12 @@ typedef struct Player {
 unsigned int total_players = 1000; //1,000
 
 //Function declarations
-float matchmaking_dist_f1();
+float matchmaking_dist_elo();
+float matchmaking_dist_trueskill();
 void input_number(int in);
+int next_mmr_elo();
+int next_mmr_trueskill();
+unsigned int nextping();
 
 //Main
 int main(int argc, char ** argv) {
@@ -62,6 +72,14 @@ int main(int argc, char ** argv) {
   }
 
   //Alloc memory
+  Player * player_arr = malloc(sizeof(Player) * total_players / mpi_size);
+  //init
+  srand48(mpi_rank);
+  for(int i = 0; i < total_players / mpi_size; ++i) {
+    player_arr[i].ping      = nextping();
+    player_arr[i].mmr       = START_MMR;
+    player_arr[i].true_mmr  = nextmmr();
+  }
 
   //Report setup time
   if(mpi_rank == 0) {
@@ -89,14 +107,57 @@ int main(int argc, char ** argv) {
     fclose(outfile);
   }
   //free memory
+  free(player_arr);
   return 0;
 }
 
 //Other functions here
 
 //distance function
-float matchmaking_dist_f1() {
+float matchmaking_dist_elo() {
   return 0.0;
+}
+
+float matchmaking_dist_mmr() {
+  return 0.0;
+}
+
+n2_cached = 0;
+double n2 = 0.0;
+float gaussian(float stdev, float mean) {
+  //Normal distribution centered around 1200
+  //With standard deviation 2000 / 7
+  //https://stackoverflow.com/questions/19944111/creating-a-gaussian-random-generator-with-a-mean-and-standard-deviation
+  if(!n2_cached) {
+    double x, y, r;
+    do {
+      x = 2.0*drand48() - 1;
+      y = 2.0*drand48() - 1;
+      r = x*x + y*y;
+    } while(r == 0 || r > 1.0);
+    double d = sqrt(-2.0 * log(r) / r);
+    double n1 = x*d;
+    n2 = y*d;
+    double res = mean + n1 * stdev;
+    n2_cached = 1;
+    return result;
+  } else {
+    n2_cached = 0;
+    return mean + n2 * stdev;
+  }
+}
+
+int next_mmr_elo() {
+  return (int) gaussian(2000.0 / 7.0, START_MMR);
+}
+
+int next_mmr_trueskill() {
+  return (int) gaussian(TODO, START_MMR);
+}
+
+unsigned int nextping() {
+  float g = gaussian(25, 50.0);
+  return max(1, (int) g);
 }
 
 void input_number(int in) {
